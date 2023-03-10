1a1b8079322ae56c57edd6ba5d2b344138d973c9
Andrei Flitea <fliteaandrei@gmail.com>
2023-03-10 17:18:51 +0200

    >Navbar component.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const menu = document.querySelectorAll('.collapsible');
 
 
 menu.forEach((item) => {
-    item.addEventListener('click', function ()  {
+    item.addEventListener("click", function ()  {
         this.classList.toggle("collapsible--expanded")
     })
 })
